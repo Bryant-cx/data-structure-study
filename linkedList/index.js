10065b3e779c00a3eae06a342af60e424f106d8d
Bryant-cx <bryant-cx@Bryant-cxs-MacBook-Pro-2.local>
2019-10-03 10:22:36 +0800

    增加链表查询

--- linkedList/index.js
+++ linkedList/index.js
@@ -12,7 +12,9 @@ class Node {
 class LinkedList {
   constructor () {
     // 头结点
-    let head = null
+    // let head = null
+    // 虚拟头节点
+    let dummyHead = new Node(null, null)
     // 链表长度
     let size = 0
 
@@ -26,41 +28,96 @@ class LinkedList {
       return size === 0
     }
 
+    // 向链表索引为index处插入节点
+    this.add = (index, val) => {
+      if (index < 0 || index > size) {
+        throw Error('Invalid index')
+      }
+
+      let prev = dummyHead
+
+      for (let i = 0; i < index; i++) {
+        prev = prev.next
+      }
+
+      // const node = new Node(val)
+      // node.next = prev.next
+      // prev.next = node
+      prev.next = new Node(val, prev.next)
+      size++
+    }
+
     // 向链表的头部添加节点
     this.addFirst = (val) => {
       // const node = new Node(val)
       // node.next = head
       // head = node
-      head = new Node(val, head)
-      size++
-    }
-
-    // 向链表索引为index处插入节点
-    this.add = (index, val) => {
-      if (index < 0 || index > size) {
-        new Error('Invalid index')
-      }
-
-      if (index === 0) {
-        this.addFirst(val)
-      } else {
-        let prev = head
-
-        for (let i = 0; i < index; i++) {
-          prev = prev.next
-        }
-
-        // const node = new Node(val)
-        // node.next = prev.next
-        // prev.next = node
-        prev.next = new Node(val, prev.next)
-        size++
-      }
+      // head = new Node(val, head)
+      this.add(0, val)
     }
 
     // 向链表结尾添加节点
     this.addLast = (val) => {
       this.add(size, val)
+    }
+
+    // 获取索引为index处的节点的值
+    this.get = (index) => {
+      if (index < 0 || index >= size) {
+        throw Error('Invalid index')
+      }
+
+      let cur = dummyHead.next
+
+      for (let i = 0; i < index; i++) {
+        cur = cur.next
+      }
+
+      return cur.val
+    }
+
+    // 获取首元素
+    this.getFirst = () => {
+      return this.get(0)
+    }
+
+    // 获取尾元素
+    this.getLast = () => {
+      return this.get(size - 1)
+    }
+
+    // 是否包含某元素
+    this.contains = (val) => {
+      let cur = dummyHead.next
+
+      while (cur) {
+        if (cur.val === val) {
+          return true
+        }
+      }
+
+      return false
+
+      // for (let cur = dummyHead.next; cur; cur = cur.next) {
+      //   if (cur.val === val) {
+      //     return true
+      //   }
+      // }
+
+      // return false
+    }
+
+    // 打印输出
+    this.toString = () => {
+      let res = ''
+      let cur = dummyHead.next
+
+      while (cur) {
+        res = res + cur.val + '-->'
+        cur = cur.next
+      }
+
+      return res + 'NULL'
     }
   }
 }
